e71b825a38cb8514cc15311cd1c194fd4073b9ef
Raghu Narayanan <nraghu@versa-networks.com>
2018-03-08 11:07:12 -0800

    Initial check in

--- src/FusionEKF.cpp
+++ src/FusionEKF.cpp
@@ -2,6 +2,7 @@
 #include "tools.h"
 #include "Eigen/Dense"
 #include <iostream>
+#define EPS 0.0001 // A very small number
 
 using namespace std;
 using Eigen::MatrixXd;
@@ -30,6 +31,9 @@ FusionEKF::FusionEKF() {
   R_radar_ << 0.09, 0, 0,
         0, 0.0009, 0,
         0, 0, 0.09;
+    
+    H_laser_ << 1, 0, 0, 0,
+    0, 1, 0, 0;
 
   /**
   TODO:
@@ -61,19 +65,51 @@ void FusionEKF::ProcessMeasurement(const MeasurementPackage &measurement_pack) {
     // first measurement
     cout << "EKF: " << endl;
     ekf_.x_ = VectorXd(4);
-    ekf_.x_ << 1, 1, 1, 1;
+   
 
     if (measurement_pack.sensor_type_ == MeasurementPackage::RADAR) {
       /**
       Convert radar from polar to cartesian coordinates and initialize state.
       */
+         cout << "EKF : First measurement RADAR" << endl;
+        /**
+         Convert radar from polar to cartesian coordinates and initialize state.
+         */
+        double rho = measurement_pack.raw_measurements_[0]; // range
+        double phi = measurement_pack.raw_measurements_[1]; // bearing
+        double rho_dot = measurement_pack.raw_measurements_[2]; // velocity of rho
+        // Coordinates convertion from polar to cartesian
+        double x = rho * cos(phi);
+        if ( x < 0.0001 ) {
+            x = 0.0001;
+        }
+        double y = rho * sin(phi);
+        if ( y < 0.0001 ) {
+            y = 0.0001;
+        }
+        double vx = rho_dot * cos(phi);
+        double vy = rho_dot * sin(phi);
+        ekf_.x_ << x, y, vx , vy;
+        
     }
     else if (measurement_pack.sensor_type_ == MeasurementPackage::LASER) {
       /**
       Initialize state.
       */
+        // No velocity and coordinates are cartesian already.
+        cout << "EKF : First measurement LASER" << endl;
+        ekf_.x_ << measurement_pack.raw_measurements_[0], measurement_pack.raw_measurements_[1], 0, 0;
+        
     }
-
+      // Initial covariance matrix
+      ekf_.P_ = MatrixXd(4, 4);
+      ekf_.P_ << 1, 0, 0, 0,
+      0, 1, 0, 0,
+      0, 0, 1000, 0,
+      0, 0, 0, 1000;
+      // Print the initialization results
+      cout << "EKF init: " << ekf_.x_ << endl;
+    previous_timestamp_ = measurement_pack.timestamp_;
     // done initializing, no need to predict or update
     is_initialized_ = true;
     return;
@@ -90,6 +126,30 @@ void FusionEKF::ProcessMeasurement(const MeasurementPackage &measurement_pack) {
      * Update the process noise covariance matrix.
      * Use noise_ax = 9 and noise_ay = 9 for your Q matrix.
    */
+    
+    float dt = (measurement_pack.timestamp_ - previous_timestamp_) / 1000000.0;    //dt - expressed in seconds
+    previous_timestamp_ = measurement_pack.timestamp_;
+    
+    float dt_2 = dt * dt;
+    float dt_3 = dt_2 * dt;
+    float dt_4 = dt_3 * dt;
+    
+    //Modify the F matrix so that the time is integrated
+    ekf_.F_ = MatrixXd(4, 4);
+    ekf_.F_ << 1, 0, dt, 0,
+    0, 1, 0, dt,
+    0, 0, 1, 0,
+    0, 0, 0, 1;
+   
+    float noise_ax = 9.0;
+    float noise_ay = 9.0;
+    //set the process covariance matrix Q
+    ekf_.Q_ = MatrixXd(4, 4);
+    ekf_.Q_ <<  dt_4/4*noise_ax, 0, dt_3/2*noise_ax, 0,
+    0, dt_4/4*noise_ay, 0, dt_3/2*noise_ay,
+    dt_3/2*noise_ax, 0, dt_2*noise_ax, 0,
+    0, dt_3/2*noise_ay, 0, dt_2*noise_ay;
+
 
   ekf_.Predict();
 
@@ -105,8 +165,13 @@ void FusionEKF::ProcessMeasurement(const MeasurementPackage &measurement_pack) {
 
   if (measurement_pack.sensor_type_ == MeasurementPackage::RADAR) {
     // Radar updates
+      ekf_.H_ = tools.CalculateJacobian(ekf_.x_);
+      ekf_.R_ = R_radar_;
+      ekf_.UpdateEKF(measurement_pack.raw_measurements_);
   } else {
-    // Laser updates
+      ekf_.H_ = H_laser_;
+      ekf_.R_ = R_laser_;
+      ekf_.Update(measurement_pack.raw_measurements_);
   }
 
   // print the output
--- src/tools.cpp
+++ src/tools.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
 #include "tools.h"
 
+#define EPS 0.0001 // A very small number
+#define EPS2 0.0000001
+
 using Eigen::VectorXd;
 using Eigen::MatrixXd;
 using std::vector;
@@ -15,6 +18,33 @@ VectorXd Tools::CalculateRMSE(const vector<VectorXd> &estimations,
   TODO:
     * Calculate the RMSE here.
   */
+    VectorXd rmse(4);
+    rmse << 0,0,0,0;
+    
+    // TODO: YOUR CODE HERE
+    
+    // check the validity of the following inputs:
+    //  * the estimation vector size should not be zero
+    //  * the estimation vector size should equal ground truth vector size
+    // ... your code here
+    if ((estimations.size() != ground_truth.size()) || (estimations.size() ==0)) {
+        cout << "Invalid data " <<  endl;
+        return rmse;
+    }
+    //accumulate squared residuals
+    for(int i=0; i < estimations.size(); ++i){
+        VectorXd residual  = estimations[i] - ground_truth[i];
+        residual = residual.array() * residual.array();
+        rmse += residual;
+    }
+    
+    //calculate the mean
+    rmse =  rmse / estimations.size();
+    
+    //calculate the squared root
+    rmse = rmse.array().sqrt();
+    //return the result
+    return rmse;
 }
 
 MatrixXd Tools::CalculateJacobian(const VectorXd& x_state) {
@@ -22,4 +52,45 @@ MatrixXd Tools::CalculateJacobian(const VectorXd& x_state) {
   TODO:
     * Calculate a Jacobian here.
   */
+    
+    MatrixXd Hj(3,4);
+    
+    if ( x_state.size() != 4 ) {
+        cout << "ERROR - CalculateJacobian () - The state vector must have size 4." << endl;
+        return Hj;
+    }
+    
+    
+    //recover state parameters
+    float px = x_state(0);
+    float py = x_state(1);
+    float vx = x_state(2);
+    float vy = x_state(3);
+    
+    
+    //check division by zero
+    // Deal with the special case problems
+    if (fabs(px) < EPS and fabs(py) < EPS){
+        px = EPS;
+        py = EPS;
+    }
+  
+    //compute the Jacobian matrix
+    float c1 = px * px + py * py;
+    
+    // Check division by zero
+    if(fabs(c1) < EPS2){
+        c1 = EPS2;
+    }
+        
+    float c2 = sqrt(c1);
+    float c3 = c1 * c2;
+    
+        
+        
+    Hj << px/c2, py / c2, 0.0,0.0,
+    -py / c1, px/c1,0.0,0.0,
+    py * (vx*py - vy*px) / c3, px * (vy*px-vx*py) / c3,px/c2, py / c2;
+   
+    return Hj;
 }
